e15baf6edca7c21e61b80ccd4002148530e8de5b
Roman Golota <roman.golota111@gmail.com>
2024-04-10 10:39:39 +0300

    Delete unused import

--- src/user/user.repository.ts
+++ src/user/user.repository.ts
@@ -1,11 +1,10 @@
 import { InjectModel } from '@nestjs/sequelize';
 import { User } from './user.model';
-import { where } from 'sequelize';
 
 export class UserRepository {
     constructor(
         @InjectModel(User) private repository: typeof User
-    ) {}
+    ) { }
 
     async getUserById(id: number) {
         const user = await this.repository.findOne({ where: { id } })
